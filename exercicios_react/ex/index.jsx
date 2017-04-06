79dc9d905d028dcd4e854f1c9a19c6bdce0eddb1
Sergio Diniz <sergiodinizsh@gmail.com>
2017-04-06 12:59:17 -0300

    ex 7

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Primeiro, Segundo} from './component'
+import SilvaFamily from './silvaFamily'
 
 ReactDOM.render(
 
-    <div>
-        <Primeiro />
-        <Segundo />
-    </div>
+    <SilvaFamily />
 
 , document.getElementById('app')) 
